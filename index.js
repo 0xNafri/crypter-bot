33dd61c588768b7e6b1c719e45f257eb51fdd570
0xNafri <101189966+0xNafri@users.noreply.github.com>
2024-02-13 14:46:09 +0800

    made simple bot reply messages

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ dotenv.config();
 
 //Create bot instance
 const bot = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
-    ]
-})
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
 //Log in bot
 bot.login(process.env.DISCORD_BOT_TOKEN);
+
+//Bot working message
+bot.on('ready', () => {
+  console.log(`${bot.user.username} is up and running!`);
+});
+
+//Reply to user message
+bot.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith("!ping")) {
+    return message.reply("I am working!");
+  }
+});
